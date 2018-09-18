4323afc7a1ab24a56f121ce1fd457f9d1b9b017e
ragnaradolf <ragnar.adolf@gmail.com>
2018-09-18 21:26:05 +0000

    Renders each item in a view cell

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+  ScrollView,
+  View,
+  Image
+} from "react-native";
 
 export default class HomeScreen extends React.Component {
   constructor(props) {
@@ -16,24 +23,56 @@ export default class HomeScreen extends React.Component {
       .then(res => this.setState({ gotData: true, concerts: res.results }));
   }
 
+  renderConcerts = concerts =>
+    concerts.map((concert, index) => {
+      return (
+        <View style={styles.concertContainer} key={index}>
+          <Image style={styles.image} source={{ uri: concert.imageSource }} />
+          <View style={styles.infoContainer}>
+            <Text style={styles.text}>{`Who? ${concert.eventDateName}`}</Text>
+            <Text style={styles.text}>{`When? ${new Date(
+              concert.dateOfShow
+            ).toLocaleString("is-IS")}`}</Text>
+            <Text style={styles.text}>{`Where? ${concert.eventHallName}`}</Text>
+          </View>
+        </View>
+      );
+    });
+
   render() {
     return (
-      <View style={styles.container}>
+      <ScrollView contentContainerStyle={styles.container}>
         {!this.state.gotData ? (
           <ActivityIndicator size="large" />
         ) : (
-          <Text>{this.state.concerts[0].eventDateName}</Text>
+          this.renderConcerts(this.state.concerts)
         )}
-      </View>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#fff",
+    padding: 8
+  },
+  concertContainer: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#fff"
+    flexDirection: "row",
+    paddingBottom: 16
+  },
+  infoContainer: {
+    paddingLeft: 8,
+    justifyContent: "space-around",
+    flex: 1
+  },
+  image: {
+    height: 125,
+    width: 125
+  },
+  text: {
+    fontSize: 12,
+    fontFamily: "space-mono"
   }
 });
